b1f8e679b86072ff029900b1b7cd4d6b6464ba02
mbzron <rupert.norman@nottingham.ac.uk>
2018-11-22 12:34:33 +0000

    made a setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gormet",
+    version="0.0.1",
+    description="A gormet coffee software suite",
+    zip_safe=False,
+    author="Rupert Norman",
+    install_requires=[],
+    author_email="rupert.norman@nottingham.ac.uk",
+    url="",
+    license="Apache", #one of many standard licenses, means anyone can use it
+    packages=find_packages(),
+)
